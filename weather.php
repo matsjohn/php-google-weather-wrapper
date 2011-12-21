131d1be8814cf1d14b54e43fe4d4fbac7f27ab16
MrControversey22003 <mrcontroversey22003@gmail.com>
2011-12-21 15:24:20 +0000

    Now throws exceptions when encountering errors, removed unnecessary type casting.

--- weather.php
+++ weather.php
@@ -1,11 +1,13 @@
 <?php
 /**
  *  Unofficial Google Weather API wrapper
- *  @author Foxhoundz
- *  @version 0.4
+ *  @author ATmega/Foxhoundz
+ *  @version 0.6
  *  @license GPL version 3
  *  @link http://code.google.com/p/php-google-weather-wrapper/wiki/Documentation
  */
+ 
+ 
   class weather {
       private $_location;
       private $_url = 'http://www.google.com/ig/api?weather=';
@@ -23,8 +25,6 @@
       }
 
       public function get_temp($type = "f") {
-          if (!$this->_isParsed)
-              return false;
 
           // User specificed celsius, return celsius
           if ($type == "c")
@@ -35,34 +35,29 @@
       }
 
       public function get_condition() {
-          if (!$this->_isParsed)
-                  return false;
+
           // provide current conditions only
           return $this->_wData['current']['condition'];
       }
 
       public function get_forecast_for_day($day) {
-          if (!$this->_isParsed)
-              return false;
+
           
           return (isset($this->_wData['forecast'][$day])) ? $this->_wData['forecast'][$day] :false;
       }
       
       public function get_forecast_assoc() {
-          if (!$this->_isParsed)
-              return false;
+
           return $this->_wData['forecast'];
       }
 
       public function get_cond_assoc() {
-          if (!$this->_isParsed)
-              return false;
+
           return $this->_wData['current'];
       }
 
       public static function to_celsius($f) {
           // Convert Fahrenheit to Celsius.
-          // I figured this would be quicker than trying to parse the XML.
           return floor(((int)$f - 32) * (5 / 9));
       }
 
@@ -72,8 +67,7 @@
           curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
           $rawXML = curl_exec($ch);
           curl_close($ch);
-          if (!$rawXML)
-              return false;
+          if (!$rawXML) throw new Exception("Unable to rerieve XML");
           return $rawXML;
       }
 
@@ -82,31 +76,30 @@
           try {
             $weather = new SimpleXMLElement($xData);
           } catch (Exception $err) {
-              // Set $lastError to getMessage()
-              $this->lastError = $err->getMessage();
-             return false;
+              // Set current exception message last getMessage()
+              throw new Exception($err->getMessage());  
           }
           // Select the current_conditions node ($cNode)
           if (!isset($weather->weather[0]->current_conditions)) {
-              $this->lastError = "Unable to find data for the specified location";
-              return false;
+              throw new Exception("Unable to find data for the specified location");
           }
+		  
           $cNode = $weather->weather[0]->current_conditions;
 
           // ========= Set up our current conditions array ====================
 
           // Tempreature - temp_f Fahrenheit, temp_c celsius - set as floats.
-          $this->_wData['current']['temp_f'] = (int)$cNode->temp_f->attributes()->data;
+          $this->_wData['current']['temp_f'] = $cNode->temp_f->attributes()->data;
           $this->_wData['current']['temp_c'] = weather::to_celsius($this->_wData['current']['temp_f']);
 
           // Condition
-          $this->_wData['current']['condition'] = (string)$cNode->condition->attributes()->data;
+          $this->_wData['current']['condition'] = $cNode->condition->attributes()->data;
 
           // Condition Icon - icon url is not absolute, append google.com
-          $this->_wData['current']['icon'] = (string)"http://www.google.com" . $cNode->icon->attributes()->data;
+          $this->_wData['current']['icon'] = "http://www.google.com" . $cNode->icon->attributes()->data;
 
           // Wind Condition
-          $this->_wData['current']['wind'] = (string)$cNode->wind_condition->attributes()->data;
+          $this->_wData['current']['wind'] = $cNode->wind_condition->attributes()->data;
 
           // ============= Set up our forecast array =============
           $fNode = $weather->weather[0]->forecast_conditions;
@@ -114,19 +107,19 @@
           // Iterate through each day of the week and create an assoc array.
           foreach ($fNode as $forecast) {
               // Get the day.
-              $day = (string)$forecast->day_of_week->attributes()->data;
+              $day = $forecast->day_of_week->attributes()->data;
 
               // Insert an array of info for that day
               $this->_wData['forecast'][$day] = array (
                   "day" => $day,
-                  "high" => (int)$forecast->high->attributes()->data,
-                  "low" => (int)$forecast->low->attributes()->data,
-                  "icon" => (string)"http://www.google.com" . $forecast->icon->attributes()->data,
-                  "condition" => (string)$forecast->condition->attributes()->data
+                  "high" => $forecast->high->attributes()->data,
+                  "low" => $forecast->low->attributes()->data,
+                  "icon" => "http://www.google.com" . $forecast->icon->attributes()->data,
+                  "condition" => $forecast->condition->attributes()->data
 				);
           } //foreach ($fNode as $forecast)
           // Let the class know wData is ready for use.
-          $this->_isParsed = true;
       } //private function parse_xml($xData)   
  }
+ 
 ?>
